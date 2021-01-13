3941b0c57dce7593f116dbc13ed5efa971ead7ab
t2 <oo_ri@naver.com>
2021-01-14 01:50:56 +0900

    Initialize GraphQL Server Setting

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-import { GraphQLServer } from 'graphql-yoga';
+const { GraphQLServer } = require('graphql-yoga');
+
+const server = new GraphQLServer({});
+
+server.start(() => console.log('GraphQL Server Start'));
