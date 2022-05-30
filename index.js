b9e5b215d4bd83a0d65f339d6de0983d87f89928
t2 <oo_ri@naver.com>
2022-05-30 10:47:26 +0900

    Reset learn-solidity Repository

--- index.js
+++
@@ -1,9 +0,0 @@
-const { GraphQLServer } = require('graphql-yoga');
-const resolvers = require('./graphql/resolvers');
-
-const server = new GraphQLServer({
-  typeDefs: './graphql/schema.graphql',
-  resolvers,
-});
-
-server.start(() => console.log('GraphQL Server Start'));
