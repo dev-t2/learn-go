bbe68831faa3781d9cba92b75b5e15462816d76b
t2 <oo_ri@naver.com>
2022-05-31 15:23:29 +0900

    Truffle and Ganache Interaction POC

--- backend/contracts/Lottery.sol
+++ backend/contracts/Lottery.sol
@@ -2,5 +2,13 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Lottery {
-  
+  address public owner;
+
+  constructor() {
+    owner = msg.sender;
+  }
+
+  function getSomeValue() public pure returns (uint256 value) {
+    return 5;
+  }
 }
