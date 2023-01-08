a1c91a069eaf43ef939564a07373fa085f98e7f3
dev-t2 <oo_ri@naver.com>
2023-01-08 17:14:43 +0900

    Function Result Update

--- main.dart
+++ main.dart
@@ -5,6 +5,7 @@ String sayHello({String name = '', required int age}) {
 int plus(int a, int b) => a + b;
 
 void main(List<String> args) {
-  print(sayHello(name: 'Austin', age: 35));
-  print(plus(1, 2));
+  String result = sayHello(name: 'Austin', age: 35);
+
+  print(result);
 }
