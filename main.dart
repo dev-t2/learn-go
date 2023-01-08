e1d4726ff8f536a0272b89b02d237694a9d94328
dev-t2 <oo_ri@naver.com>
2023-01-08 16:45:03 +0900

    Plus Function Update

--- main.dart
+++ main.dart
@@ -2,6 +2,9 @@ String sayHello(String name) {
   return 'Hello $name, Nice to meet you!';
 }
 
+int plus(int a, int b) => a + b;
+
 void main(List<String> args) {
   print(sayHello('Austin'));
+  print(plus(1, 2));
 }
