ce04bc7539663b4c54eb460a18e94ce2708e5ba9
dev-t2 <oo_ri@naver.com>
2023-06-15 22:45:53 +0900

    02. Variable > Example 05 Update

--- 02-variable/ex03/main.go
+++ 02-variable/ex03/main.go
@@ -5,11 +5,13 @@ import "fmt"
 func main() {
 	var num1 = 1
 	var num2 = 2.5
+
 	var num3 int = int(num2) // 2
 	var num4 float64 = float64(num1 * num3) // 1 * 2 = 2
-	var num5 int64 = 5
 
+	var num5 int64 = 5
 	var num6 int64 = int64(num4) * num5 // 2 * 5 = 10
+	
 	var num7 int = int(num2 * 7) // 2.5 * 7 = 17
 	var num8 int = int(num2) * 8 // 2 * 8 = 16
 
