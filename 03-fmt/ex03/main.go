19f9e192a32583d96f5d6404794935b578293596
dev-t2 <oo_ri@naver.com>
2023-06-17 14:09:29 +0900

    03. FMT > Example 03 Update

--- 03-fmt/ex03/main.go
+++ 03-fmt/ex03/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var str = "Hello\tGo\tWorld\n\"Go\" is Awesome!\n"
+	var str = "Hello\tGo\t\tWorld\n\"Go\" is Awesome!\n"
 
 	fmt.Print(str)
 	fmt.Printf("%s", str)
