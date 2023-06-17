9eca1220ef881e3bb155e73a0ddfac7c947d2bba
dev-t2 <oo_ri@naver.com>
2023-06-17 16:44:09 +0900

    03. FMT > Example 04 Update

--- 03-fmt/ex04/main.go
+++ 03-fmt/ex04/main.go
@@ -6,11 +6,11 @@ func main() {
 	var num1 int
 	var num2 int
 
-	var num, err = fmt.Scan(&num1, &num2)
+	var n, err = fmt.Scan(&num1, &num2)
 
 	if err == nil {
-		fmt.Println(num, num1, num2)
+		fmt.Println(n, num1, num2)
 	} else {
-		fmt.Println(num, err)
+		fmt.Println(n, err)
 	}
 }
