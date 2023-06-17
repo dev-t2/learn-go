5975131bb1ec216fb0eb8b44e46791b122ec2006
dev-t2 <oo_ri@naver.com>
2023-06-17 13:52:24 +0900

    03. FMT > Example 02 Update

--- 03-fmt/ex02/main.go
+++ 03-fmt/ex02/main.go
@@ -10,4 +10,14 @@ func main() {
 	fmt.Printf("%5d, %5d, %5d\n", num1, num2, num3)
 	fmt.Printf("%05d, %05d, %05d\n", num1, num2, num3)
 	fmt.Printf("%-5d, %-5d, %-5d\n", num1, num2, num3)
+
+	var num4 = 123.45678
+
+	fmt.Printf("%08.2f\n", num4)
+	fmt.Printf("%08.2g\n", num4)
+	fmt.Printf("%8.5g\n", num4)
+
+	var num5 = 3.14
+
+	fmt.Printf("%f\n", num5)
 }
