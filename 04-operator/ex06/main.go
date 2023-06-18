4c42ace70790e360e6c3d9e22c14d68ccf169fb0
dev-t2 <oo_ri@naver.com>
2023-06-18 18:03:37 +0900

    04. Operator > Example 06 and 07 Update

--- 04-operator/ex06/main.go
+++ 04-operator/ex06/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"math"
-	"math/big"
 )
 
 func equal(a, b float64) bool {
@@ -23,12 +22,4 @@ func main() {
 	num3 = 0.0000000000007
 
 	fmt.Printf("%g == %g: %v\n", num1 + num2, num3, equal(num1 + num2, num3))
-
-	var num4, _ = new(big.Float).SetString("0.1")
-	var num5, _ = new(big.Float).SetString("0.2")
-	var num6, _ = new(big.Float).SetString("0.3")
-	var num7 = new(big.Float).Add(num4, num5)
-
-	fmt.Println(num4, num5, num6, num7)
-	fmt.Println(num6.Cmp(num7))
 }
