94bb72c2c8857874dad5dd890823e1a2aa0694b6
dev-t2 <oo_ri@naver.com>
2023-06-18 17:55:35 +0900

    04. Operator > Example 06 Update

---
+++ 04-operator/ex06/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+const epsilon = 0.000001
+
+func equal(a, b float64) bool {
+	return math.Nextafter(a, b) == b
+}
+
+func main() {
+	var num1 = 0.1
+	var num2 = 0.2
+	var num3 = 0.3
+
+	fmt.Printf("%0.18f + %0.18f = %0.18f\n", num1, num2, num1 + num2)
+	fmt.Printf("%0.18f == %0.18f: %v\n", num1 + num2, num3, equal(num1 + num2, num3))
+
+	num1 = 0.0000000000004
+	num2 = 0.0000000000002
+	num3 = 0.0000000000007
+
+	fmt.Printf("%g == %g: %v", num1 + num2, num3, equal(num1 + num2, num3))
+}
