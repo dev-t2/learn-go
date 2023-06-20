c6b06032ff1d46ae878af9cdaedcc4d5ce3327e0
dev-t2 <oo_ri@naver.com>
2023-06-20 23:00:32 +0900

    05. Function > Example 03 Update

--- 05-function/ex03/main.go
+++ 05-function/ex03/main.go
@@ -2,20 +2,30 @@ package main
 
 import "fmt"
 
-func Divide(a, b int) (int, bool) {
+func Divide(a, b int) (result int, success bool) {
 	if b == 0 {
-		return 0, false
+		// return 0, false
+
+		result = 0
+		success = false
+
+		return
 	}
 
-	return a / b, true
+	// return a / b, true
+
+	result = a / b
+	success = true
+
+	return
 }
 
 func main() {
-	var num, success = Divide(9, 3)
+	var result, success = Divide(9, 3)
 
-	fmt.Println(num, success)
+	fmt.Println(result, success)
 
-	num, success = Divide(9, 0)
+	result, success = Divide(9, 0)
 
-	fmt.Println(num, success)
+	fmt.Println(result, success)
 }
