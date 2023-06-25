7ab0b6d53878e8715d0438e850ce98e706cd771d
dev-t2 <oo_ri@naver.com>
2023-06-25 09:39:49 +0900

    08. Switch > Example 01 Update

--- 08-switch/ex01/main.go
+++ 08-switch/ex01/main.go
@@ -29,4 +29,11 @@ func main() {
 	default: 
 		fmt.Println("Switch: num > 4")
 	}
+
+	switch num {
+	case 1, 2:
+		fmt.Println("num == 1 or num == 2")
+	case 3, 4, 5:
+		fmt.Println("num == 3 or num == 4 or num == 5")
+	}
 }
