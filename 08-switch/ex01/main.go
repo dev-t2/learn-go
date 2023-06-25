2525e24e1167121946fbacdedeecf726ecf10e91
dev-t2 <oo_ri@naver.com>
2023-06-25 09:27:37 +0900

    08. Switch > Example 01 Update

--- 08-switch/ex01/main.go
+++ 08-switch/ex01/main.go
@@ -5,16 +5,28 @@ import "fmt"
 func main() {
 	var num = 3
 
+	if num == 1 {
+		fmt.Println("If: num == 1")
+	} else if num == 2 {
+		fmt.Println("If: num == 2")
+	} else if num == 3 {
+		fmt.Println("If: num == 3")
+	} else if num == 4 {
+		fmt.Println("If: num == 3")
+	} else {
+		fmt.Println("If: num > 4")
+	}
+
 	switch num {
 	case 1:
-		fmt.Println("num == 1")
+		fmt.Println("Switch: num == 1")
 	case 2:
-		fmt.Println("num == 2")
+		fmt.Println("Switch: num == 2")
 	case 3:
-		fmt.Println("num == 3")
+		fmt.Println("Switch: num == 3")
 	case 4:
-		fmt.Println("num == 4")
+		fmt.Println("Switch: num == 4")
 	default: 
-		fmt.Println("num > 4")
+		fmt.Println("Switch: num > 4")
 	}
 }
