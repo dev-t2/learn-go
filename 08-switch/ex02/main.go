1c0cd58371773c505a43b8e39f7f3e90a5fa0159
dev-t2 <oo_ri@naver.com>
2023-06-25 09:45:14 +0900

    08. Switch > Example 02 Update

--- 08-switch/ex02/main.go
+++ 08-switch/ex02/main.go
@@ -3,12 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	var num = 4
+	var num = 19
 
-	switch num {
-	case 1, 2:
-		fmt.Println("num == 1 or num == 2")
-	case 3, 4, 5:
-		fmt.Println("num == 3 or num == 4 or num == 5")
+	switch true {
+	case num < 10 || num > 30:
+		fmt.Println("num < 10 || num >= 30")
+	case num >= 10 && num < 20:
+		fmt.Println("num >= 10 && num < 20")
+	case num >= 15 && num < 25:
+		fmt.Println("num >= 15 && num < 25")
+	default:
+		fmt.Println("num >= 25 && num < 30")
 	}
 }
