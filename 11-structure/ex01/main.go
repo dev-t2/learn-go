4f71919c29b45184f370597be5d3732e58d5cd08
dev-t2 <oo_ri@naver.com>
2023-07-05 20:15:19 +0900

    11. Structure > Example 01 Update

--- 11-structure/ex01/main.go
+++ 11-structure/ex01/main.go
@@ -11,7 +11,17 @@ func main() {
 	var user User
 
 	user.Id = 0
-	user.Name = "TypeScript"
+	user.Name = "Hello"
+
+	fmt.Println("id:", user.Id)
+	fmt.Println("name:", user.Name)
+
+	user = User {1, "World"}
+
+	fmt.Println("id:", user.Id)
+	fmt.Println("name:", user.Name)
+
+	user = User {Name: "Hello World"}
 
 	fmt.Println("id:", user.Id)
 	fmt.Println("name:", user.Name)
