6f22c362ef2e0d9eb9776be3d552c5c3a6a1d577
dev-t2 <oo_ri@naver.com>
2023-07-07 21:58:46 +0900

    11. Structure > Example 04 Update

---
+++ 11-structure/ex04/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"unsafe"
+)
+
+type User1 struct {
+	A int8
+	B int
+	C int8
+	D int
+	E int8
+}
+
+type User2 struct {
+	A int8
+	B int8
+	C int8
+	D int
+	E int
+}
+
+func main() {
+	var user1 = User1{1, 2, 3, 4, 5}
+
+	fmt.Println(unsafe.Sizeof(user1))
+
+	var user2 = User2{1, 2, 3, 4, 5}
+
+	fmt.Println(unsafe.Sizeof(user2))
+}
