e3b6912649b8908a8097c6321b1ee67c2f291884
dev-t2 <oo_ri@naver.com>
2023-07-07 21:51:33 +0900

    11. Structure > Example 03 Update

--- 11-structure/ex03/main.go
+++ 11-structure/ex03/main.go
@@ -1,19 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unsafe"
+)
 
 type User struct {
 	Id   int
-	Name string
+	Height float64
 }
 
 func PrintUser(user User) {
-	fmt.Printf("아이디: %d, 이름: %s", user.Id, user.Name)
+	fmt.Printf("아이디: %d, 이름: %.1f\n", user.Id, user.Height)
 }
 
 func main() {
-	var user1 = User{1, "Go"}
+	var user1 = User{1, 173.0}
 	var user2 = user1
 
 	PrintUser(user2)
+
+	fmt.Println(unsafe.Sizeof(user2))
 }
