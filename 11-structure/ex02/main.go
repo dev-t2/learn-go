24942a2731f94db8631a1026dbbad1535578a198
dev-t2 <oo_ri@naver.com>
2023-07-07 21:31:30 +0900

    11. Structure > Example 02 Update

--- 11-structure/ex02/main.go
+++ 11-structure/ex02/main.go
@@ -12,12 +12,21 @@ type Owner struct {
 	Level    int
 }
 
+type Customer struct {
+	User
+	Level int
+}
+
 func main() {
 	var user = User{0, "Go"}
 	
 	fmt.Printf("아이디: %d, 이름: %s\n", user.Id, user.Name)
 	
-	var owner = Owner{user, 4}
+	var owner = Owner{user, 1}
 
 	fmt.Printf("아이디: %d, 이름: %s, 레벨: %d\n", owner.UserInfo.Id, owner.UserInfo.Name, owner.Level)
+
+	var customer = Customer{user, 2}
+	
+	fmt.Printf("아이디: %d, 이름: %s, 레벨: %d\n", customer.Id, customer.Name, customer.Level)
 }
