2e01645945e20c54a3edc5c2c091c8af74197e94
dev-t2 <oo_ri@naver.com>
2023-07-08 19:59:57 +0900

    12. Pointer > Example 03 Update

---
+++ 12-pointer/ex03/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+type Data struct {
+	data  int
+	datas [100]int
+}
+
+func UpdateData(d Data) {
+	d.data = 99
+	d.datas[99] = 99
+}
+
+func main() {
+	var d Data
+
+	UpdateData(d)
+
+	fmt.Printf("data = %d\n", d.data)
+	fmt.Printf("datas[99] = %d\n", d.datas[99])
+}
