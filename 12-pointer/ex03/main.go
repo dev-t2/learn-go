92a9ba979a6a697a9106c801779a4a9f516f6b89
dev-t2 <oo_ri@naver.com>
2023-07-08 20:06:20 +0900

    12. Pointer > Example 03 Update

--- 12-pointer/ex03/main.go
+++ 12-pointer/ex03/main.go
@@ -12,11 +12,21 @@ func UpdateData(d Data) {
 	d.datas[99] = 99
 }
 
+func UpdatePointData(d *Data) {
+	d.data = 99
+	d.datas[99] = 99
+}
+
 func main() {
 	var d Data
 
 	UpdateData(d)
 
+	fmt.Printf("data = %d\n", d.data)
+	fmt.Printf("datas[99] = %d\n", d.datas[99])
+
+	UpdatePointData(&d)
+	
 	fmt.Printf("data = %d\n", d.data)
 	fmt.Printf("datas[99] = %d\n", d.datas[99])
 }
