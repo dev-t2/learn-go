db77ac5ee5dd7c0f266a7b3447ff4ace0d313922
dev-t2 <oo_ri@naver.com>
2023-07-11 21:30:35 +0900

    14. Slice > Example 01 Update

--- 14-slice/ex01/main.go
+++ 14-slice/ex01/main.go
@@ -8,4 +8,18 @@ func main() {
 	if len(slice) == 0 {
 		fmt.Println(slice)
 	}
+
+	slice = append(slice, 0)
+
+	fmt.Println(slice)
+
+	for i := 0; i < 10; i++ {
+		slice = append(slice, i + 1)
+	}
+
+	fmt.Println(slice)
+
+	slice = append(slice, 11, 12, 13, 14, 15)
+
+	fmt.Println(slice)
 }
