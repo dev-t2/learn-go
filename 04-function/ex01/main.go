64c75f51b6109d982072697fa800ff829e4e0e8b
dev-t2 <oo_ri@naver.com>
2023-07-23 19:40:42 +0900

    04. Function > Example 01 Update

--- 04-function/ex01/main.go
+++ 04-function/ex01/main.go
@@ -2,33 +2,24 @@ package main
 
 import "fmt"
 
-func Add(a int, b int) int {
-	return a + b
+func plus(num1 int, num2 int) int {
+	return num1 + num2
 }
 
-func AverageScore(math int, english int, history int) {
+func averageScore(math int, english int, history int) {
 	var total = math + english + history
 	var average = total / 3
 
 	fmt.Println("평균 점수는", average, "점 입니다.")
 }
 
-func PrintString(nums ...int) {
-	fmt.Println(nums)
-}
-
 func main() {
-	var num = Add(1, 2)
+	var result = plus(1, 2)
 
-	fmt.Println(num)
-
-	fmt.Println()
-
-	AverageScore(80, 74, 95)
-	AverageScore(88, 92, 53)
-	AverageScore(78, 73, 78)
+	fmt.Println(result)
 
 	fmt.Println()
 
-	PrintString(1, 2, 3, 4, 5)
+	averageScore(80, 74, 95)
+	averageScore(88, 92, 53)
 }
