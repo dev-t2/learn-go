6f3c98968688c2c4e131cbfaed746e7a4d8742a5
dev-t2 <oo_ri@naver.com>
2023-08-21 23:44:15 +0900

    18. Error > Example 04 Update

--- 18-error/ex04/main.go
+++ 18-error/ex04/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 func readNextInt(scanner *bufio.Scanner) (int, int, error) {
@@ -12,11 +14,57 @@ func readNextInt(scanner *bufio.Scanner) (int, int, error) {
 	}
 
 	var word = scanner.Text()
-	var num, err = strconv.Atoi(word)
+
+	num, err := strconv.Atoi(word)
 
 	if err != nil {
-		return 0, 0, fmt.Errorf("word: %s, err: %w", word, err)
+		return 0, 0, fmt.Errorf("Word: %s, Error: %w", word, err)
 	}
 
 	return num, len(word), nil
+}
+
+func MultipleFromString(str string) (int, error) {
+	var scanner = bufio.NewScanner(strings.NewReader(str))
+
+	scanner.Split(bufio.ScanWords)
+
+	var pos = 0
+
+	a, n, err := readNextInt(scanner)
+
+	if err != nil {
+		return 0, fmt.Errorf("Position: %d, Error: %w", pos, err)
+	}
+
+	pos += n + 1
+
+	b, n, err := readNextInt(scanner)
+
+	if err != nil {
+		return 0, fmt.Errorf("Position: %d, Error: %w", pos, err)
+	}
+
+	return a * b, nil
+}
+
+func readEq(eq string) {
+	rst, err := MultipleFromString(eq)
+
+	if err == nil {
+		fmt.Println(rst)
+	} else {
+		fmt.Println(err)
+
+		var numError *strconv.NumError
+
+		if errors.As(err, &numError) {
+			fmt.Println(numError)
+		}
+	}
+}
+
+func main() {
+	readEq("123 3")
+	readEq("123 abc")
 }
