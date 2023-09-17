efaf602c94839e5aabf5e03de57943bf073c6a35
dev-t2 <oo_ri@naver.com>
2023-09-17 15:56:41 +0900

    01. Syntax > 02. Type Update

--- 01-syntax/02-type/main.go
+++ 01-syntax/02-type/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
 	fmt.Println("Hello,\nGo!")
@@ -8,10 +11,45 @@ func main() {
 	fmt.Println("Quotes: \"\"")
 	fmt.Println("Backslash: \\")
 
+	fmt.Println()
+
 	fmt.Println('A')
 	fmt.Println('b')
 	fmt.Println('가')
 	fmt.Println('\t')
 	fmt.Println('\n')
 	fmt.Println('\\')
+
+	fmt.Println()
+
+	fmt.Println(true)
+	fmt.Println(false)
+
+	fmt.Println()
+
+	fmt.Println(42)
+	fmt.Println(3.141592)
+
+	fmt.Println()
+
+	fmt.Println(1 + 2)
+	fmt.Println(5.4 - 2.2)
+	fmt.Println(3 * 4)
+	fmt.Println(7.5 / 5)
+
+	fmt.Println()
+
+	fmt.Println(4 < 6)
+	fmt.Println(4 > 6)
+	fmt.Println(2 + 2 == 5)
+	fmt.Println(2 + 2 != 5)
+	fmt.Println(4 <= 6)
+	fmt.Println(4 >= 4)
+
+	fmt.Println()
+
+	fmt.Println(reflect.TypeOf(42))
+	fmt.Println(reflect.TypeOf(3.141592))
+	fmt.Println(reflect.TypeOf(true))
+	fmt.Println(reflect.TypeOf("Hello, Go!"))
 }
