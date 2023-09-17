ab15e33778811b730eec0d706bd7bbdd60bda5b5
dev-t2 <oo_ri@naver.com>
2023-09-17 13:46:01 +0900

    01. Syntax > 02. Type Update

--- 01-syntax/02-type/main.go
+++ 01-syntax/02-type/main.go
@@ -7,4 +7,11 @@ func main() {
 	fmt.Println("Hello,\tGo!")
 	fmt.Println("Quotes: \"\"")
 	fmt.Println("Backslash: \\")
+
+	fmt.Println('A')
+	fmt.Println('b')
+	fmt.Println('가')
+	fmt.Println('\t')
+	fmt.Println('\n')
+	fmt.Println('\\')
 }
