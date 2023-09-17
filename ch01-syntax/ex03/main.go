b41369c9fc08ddf2a62bd7515c1d71cfb1b0a20d
dev-t2 <oo_ri@naver.com>
2023-09-17 11:10:19 +0900

    Chapter 01 Syntax > Example 03 Update

---
+++ ch01-syntax/ex03/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, Go!")
+	fmt.Println("Hello,\nGo!")
+	fmt.Println("Hello,\tGo!")
+	fmt.Println("Quotes: \"\"")
+	fmt.Println("Backslash: \\")
+}
