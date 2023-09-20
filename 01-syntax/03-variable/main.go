752583892a1a7d8730dcadcdc9dc5fa05ee02bf0
dev-t2 <oo_ri@naver.com>
2023-09-20 21:53:05 +0900

    01. Syntax > 03. Variable Update

--- 01-syntax/03-variable/main.go
+++ 01-syntax/03-variable/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"reflect"
+)
 
+func main() {
+	// var quantity int
+	// var length, width float64
+	// var customerName string
+
+	// quantity = 4
+	// length, width = 1.2, 3.4
+	// customerName = "Austin"
+
+	var quantity = 4
+	var length, width = 1.2, 3.4
+	var customerName = "Austin"
+
+	fmt.Println(reflect.TypeOf(quantity))
+	fmt.Println(reflect.TypeOf(length))
+	fmt.Println(reflect.TypeOf(width))
+	fmt.Println(reflect.TypeOf(customerName))
 }
