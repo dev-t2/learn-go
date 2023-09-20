df3cd1f57bad9557623fc298cd9928c59b617c70
dev-t2 <oo_ri@naver.com>
2023-09-20 21:57:25 +0900

    01. Syntax > 03. Variable Update

--- 01-syntax/03-variable/main.go
+++ 01-syntax/03-variable/main.go
@@ -22,4 +22,16 @@ func main() {
 	fmt.Println(reflect.TypeOf(length))
 	fmt.Println(reflect.TypeOf(width))
 	fmt.Println(reflect.TypeOf(customerName))
+
+	fmt.Println()
+
+	var myInt int
+	var myFloat float64
+	var myString string
+	var myBool bool
+
+	fmt.Println(myInt)
+	fmt.Println(myFloat)
+	fmt.Println(myString)
+	fmt.Println(myBool)
 }
