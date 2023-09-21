bb79b0ed60af6144a8bafe1e1bcbf390cd1582aa
dev-t2 <oo_ri@naver.com>
2023-09-21 22:19:12 +0900

    02. Conditionals and Loops > 01. Method Update

--- 02-conditionals-and-loops/01-method/main.go
+++ 02-conditionals-and-loops/01-method/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strings"
+	"time"
+)
 
+func main() {
+	now := time.Now()
+	year := now.Year()
+
+	fmt.Println(year)
+
+	fmt.Println()
+
+	broken := "G# r#cks!"
+	replacer := strings.NewReplacer("#", "o")
+	fixed := replacer.Replace(broken)
+
+	fmt.Println(fixed)
 }
