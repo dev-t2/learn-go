94f13b876d0c6d29efd7dc2f0bfa5134befd3905
dev-t2 <oo_ri@naver.com>
2023-09-23 12:18:44 +0900

    Chapter 02 > 02. Conditional Update

---
+++ ch02/02-conditional/main.go
@@ -0,0 +1,61 @@
+package main
+
+import "fmt"
+
+func main() {	
+	if true {
+		fmt.Println("true")
+	}
+
+	if false {
+		fmt.Println("false")
+	}
+
+  if 1 == 1 {
+		fmt.Println("1 == 1")
+	}
+
+	if 1 > 2 {
+		fmt.Println("1 > 2")
+	}
+
+	if 1 < 2 {
+		fmt.Println("1 < 2")
+	}
+
+	if 1 >= 2 {
+		fmt.Println("1 >= 2")
+	}
+
+	if 2 <= 2 {
+		fmt.Println("2 <= 2")
+	}
+
+	if 2 != 2 {
+		fmt.Println("2 != 2")
+	}
+
+	if !true {
+		fmt.Println("!true")
+	}
+
+	if !false {
+		fmt.Println("!false")
+	}
+
+	if true && 1 == 1 {
+		fmt.Println("true && 1 == 1")
+	}
+
+	if false && 1 == 1 {
+		fmt.Println("false && 1 == 1")
+	}
+
+	if false || 1 == 1 {
+		fmt.Println("false || 1 == 1")
+	}
+
+	if false || 1 != 1 {
+		fmt.Println("false || 1 != 1")
+	}
+}
