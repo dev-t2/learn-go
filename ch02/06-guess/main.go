87d9007e82ed7823f887fe86a7d2ed84424f5c7d
dev-t2 <oo_ri@naver.com>
2023-09-23 21:02:58 +0900

    Chapter 02 > 06. Guess Update

--- ch02/06-guess/main.go
+++ ch02/06-guess/main.go
@@ -1,15 +1,44 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"math/rand"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	target := rand.Intn(100) + 1
 
-	fmt.Println("I've chosen a random number between 1 and 100.")
+	fmt.Println("I've chosen a random number between 1 and 100")
 	fmt.Println("Can you guess it?")
+	fmt.Println()
+
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Make a guess: ")
+
+	input, err := reader.ReadString('\n')
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	input = strings.TrimSpace(input)
+	guess, err := strconv.Atoi(input)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	fmt.Println(target)
+
+	if guess < target {
+		fmt.Println("Your guess was LOW")
+	} else if guess > target {
+		fmt.Println("Your guess was HIGH")
+	}
 }
