9abc83887d454b636846a14fdb65cb40ae30752a
dev-t2 <oo_ri@naver.com>
2023-09-23 20:55:10 +0900

    Chapter 02 > 06. Guess Update

--- ch02/06-guess/main.go
+++ ch02/06-guess/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math/rand"
+)
 
+func main() {
+	target := rand.Intn(100) + 1
+
+	fmt.Println("I've chosen a random number between 1 and 100.")
+	fmt.Println("Can you guess it?")
+
+	fmt.Println(target)
 }
