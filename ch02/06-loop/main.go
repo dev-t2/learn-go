44aa5763121338764d6ce0a6578d6489e47f98b0
dev-t2 <oo_ri@naver.com>
2023-09-23 23:25:56 +0900

    Chapter 02 > 06. Loop Update

--- ch02/06-loop/main.go
+++ ch02/06-loop/main.go
@@ -1,5 +1,75 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	for i := 1; i <= 3; i++ {
+		fmt.Println(i)
+	}
+
+	fmt.Println()
+
+	for i := 3; i >= 1; i-- {
+		fmt.Println(i)
+	}
+
+	fmt.Println()
+
+	for i := 1; i <= 5; i += 2 {
+		fmt.Println(i)
+	}
+
+	fmt.Println()
+
+	for i := 15; i >= 5; i -= 5 {
+		fmt.Println(i)
+	}
+
+	fmt.Println()
+
+	i := 1
+
+	for i <= 3 {
+		fmt.Println(i)
+
+		i++
+	}
+
+	fmt.Println()
+
+	i = 3
+
+	for i >= 1 {
+		fmt.Println(i)
+
+		i--
+	}
+
+	fmt.Println()
+
+	for x := 1; x <= 3; x++ {
+		y := x + 1
+
+		fmt.Println(y)
+	}
+
+	// fmt.Println(y)
+
+	fmt.Println()
+
+	for x := 1; x <= 3; x++ {
+		fmt.Println(x)
+	}
+
+	// fmt.Println(x)
+
+	fmt.Println()
+
+	var x int
+
+	for x = 1; x <= 3; x++ {
+		fmt.Println(x)
+	}
+
+	fmt.Println(x)
 }
