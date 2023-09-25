69d8eea860218265299a263fa16c642948cb18e8
dev-t2 <oo_ri@naver.com>
2023-09-25 21:40:31 +0900

    Chapter 03 > 01. Printf Update

--- ch03/01-printf/main.go
+++ ch03/01-printf/main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Println(1.0 / 3.0)
+	
+	fmt.Printf("%0.2f\n", 1.0 / 3.0)
+
+	result := fmt.Sprintf("%0.2f\n", 1.0 / 3.0)
+
+	fmt.Printf(result)
 }
