1f93e8374d72a78d82aa30834e99401cd09b8177
dev-t2 <oo_ri@naver.com>
2023-09-27 00:53:18 +0900

    Chapter 01 > 02. Types Update

--- ch01/02-types/main.go
+++ ch01/02-types/main.go
@@ -34,15 +34,15 @@ func main() {
 
 	fmt.Println(4 < 6)
 	fmt.Println(4 > 6)
-	fmt.Println(2+2 == 5)
-	fmt.Println(2+2 != 5)
+	fmt.Println(2 + 2 == 5)
+	fmt.Println(2 + 2 != 5)
 	fmt.Println(4 <= 6)
 	fmt.Println(4 >= 4)
 
 	fmt.Println()
 
+	fmt.Println(reflect.TypeOf("Hello, Go!"))
+	fmt.Println(reflect.TypeOf(true))
 	fmt.Println(reflect.TypeOf(42))
 	fmt.Println(reflect.TypeOf(3.141592))
-	fmt.Println(reflect.TypeOf(true))
-	fmt.Println(reflect.TypeOf("Hello, Go!"))
 }
