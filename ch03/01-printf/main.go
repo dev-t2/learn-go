2639ea752ea6d434c9535a7e635501ef2c7c9c5f
dev-t2 <oo_ri@naver.com>
2023-09-28 02:11:04 +0900

    Chapter 03 > 01. Printf Update

--- ch03/01-printf/main.go
+++ ch03/01-printf/main.go
@@ -10,4 +10,15 @@ func main() {
 	result := fmt.Sprintf("%0.2f\n", 1.0 / 3.0)
 
 	fmt.Printf(result)
+
+	fmt.Println()
+
+	fmt.Printf("Float: %f\n", 3.14)
+	fmt.Printf("Integer: %d\n", 15)
+	fmt.Printf("String: %s\n", "Hello, Go!")
+	fmt.Printf("Boolean: %t\n", false)
+	fmt.Printf("Values: %v %v %v\n", 1.2, "\t", true)
+	fmt.Printf("Values: %v %#v %#v\n", 1.2, "\t", true)
+	fmt.Printf("Types: %T %T %T\n", 1.2, "\t", true)
+	fmt.Printf("Percent Sign: %%\n")
 }
