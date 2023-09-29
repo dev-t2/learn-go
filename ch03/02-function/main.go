fcb6824ee6e311cfef5468c32181ef70635454e9
dev-t2 <oo_ri@naver.com>
2023-09-29 22:24:23 +0900

    Chapter 03 > 02. Function Update

--- ch03/02-function/main.go
+++ ch03/02-function/main.go
@@ -6,6 +6,16 @@ func sayHi() {
 	fmt.Println("Hi!")
 }
 
+func repeatLine(line string, times int) {
+	for i := 0; i < times; i++ {
+		fmt.Println(line)
+	}
+}
+
 func main() {
 	sayHi()
+
+	fmt.Println()
+
+	repeatLine("hello", 3)
 }
