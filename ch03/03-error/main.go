c24ae6f69df1caf98cc456d9e6885e4b098c2d02
dev-t2 <oo_ri@naver.com>
2023-09-30 00:44:04 +0900

    Chapter 03 > 03. Error Update

--- ch03/03-error/main.go
+++ ch03/03-error/main.go
@@ -3,8 +3,16 @@ package main
 import (
 	"errors"
 	"fmt"
-	"log"
+	"math"
 )
+
+func squareRoot(num float64) (float64, error) {
+	if num < 0 {
+		return 0, fmt.Errorf("Can't get square root of negative number")
+	}
+
+	return math.Sqrt(num), nil
+}
 
 func main() {
 	err := errors.New("Height can't be negative")
@@ -16,6 +24,16 @@ func main() {
 
 	err = fmt.Errorf("Height of %.2f is invalid", -2.33333)
 
+	fmt.Println(err.Error())
 	fmt.Println(err)
-	log.Fatal(err)
+
+	fmt.Println()
+
+	root, err := squareRoot(-9.3)
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("%.3f", root)
+	}
 }
