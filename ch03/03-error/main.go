be50c9934ce66fc658f83ea5dbb549bdd8910d37
dev-t2 <oo_ri@naver.com>
2023-09-30 00:53:27 +0900

    Chapter 03 > 03. Error Update

--- ch03/03-error/main.go
+++ ch03/03-error/main.go
@@ -6,6 +6,14 @@ import (
 	"math"
 )
 
+func divide(dividend, divisor float64) (float64, error) {
+	if divisor == 0 {
+		return 0, errors.New("Can't divide by 0")
+	}
+
+	return dividend / divisor, nil
+}
+
 func squareRoot(num float64) (float64, error) {
 	if num < 0 {
 		return 0, fmt.Errorf("Can't get square root of negative number")
@@ -29,11 +37,21 @@ func main() {
 
 	fmt.Println()
 
+	result, err := divide(5.6, 0)
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("%.2f\n", result)
+	}
+
+	fmt.Println()
+
 	root, err := squareRoot(-9.3)
 
 	if err != nil {
 		fmt.Println(err)
 	} else {
-		fmt.Printf("%.3f", root)
+		fmt.Printf("%.3f\n", root)
 	}
 }
