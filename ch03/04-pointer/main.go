5e64e8f3005136a6ac0d4980d7aab4003830f53b
dev-t2 <oo_ri@naver.com>
2023-09-30 15:15:19 +0900

    Chapter 03 > 04. Pointer Update

--- ch03/04-pointer/main.go
+++ ch03/04-pointer/main.go
@@ -1,10 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
 	amount := 6
 
 	fmt.Println(amount)
 	fmt.Println(&amount)
+
+	fmt.Println()
+
+	var myInt int
+
+	fmt.Println(&myInt)
+	fmt.Println(reflect.TypeOf(&myInt))
+
+	fmt.Println()
+
+	// var myIntPointer *int
+	// myIntPointer = &myInt
+
+	myIntPointer := &myInt
+
+	fmt.Println(myIntPointer)
 }
