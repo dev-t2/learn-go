29fb708268f1cf2372f5d0cf2a4bcf0507c6a609
dev-t2 <oo_ri@naver.com>
2023-10-01 23:12:18 +0900

    Chapter 06 > 01. Slice and 03. Average Update

--- ch06/03-average/main.go
+++ ch06/03-average/main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"fmt"
-	"learn-go/ch05/03-average/datafile"
+	"learn-go/ch06/03-average/datafile"
 	"log"
 )
 
 func main() {
-	nums, err := datafile.GetFloats("ch05/03-average/data.txt")
+	nums, err := datafile.GetFloats("ch06/03-average/data.txt")
 
 	if err != nil {
 		log.Fatal(err)
