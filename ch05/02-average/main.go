8d3c48fb162ea89ff2dfedb2c9c6852a5c5bd32e
dev-t2 <oo_ri@naver.com>
2023-10-01 20:50:22 +0900

    Chapter 05 > 02. Average Update

--- ch05/02-average/main.go
+++ ch05/02-average/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	nums := [3]float64{71.8, 56.2, 89.5}
+	sum := 0.0
+
+	for _, num := range nums {
+		sum += num
+	}
+
+	length := float64(len(nums))
+
+	fmt.Printf("Average: %0.2f\n", sum / length)
 }
