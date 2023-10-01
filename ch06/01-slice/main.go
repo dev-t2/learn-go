6dd0794e113e4adbb9ea1559633331526acb9bc2
dev-t2 <oo_ri@naver.com>
2023-10-01 23:43:11 +0900

    Chapter 06 > 01. Slice Update

--- ch06/01-slice/main.go
+++ ch06/01-slice/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	// var notes []string
+	// notes = make([]string, 7)
+
+	notes := make([]string, 7)
+
+	notes[0] = "do"
+	notes[1] = "re"
+	notes[2] = "mi"
+
+	fmt.Println(notes[0], notes[1], notes[2])
+
+	fmt.Println()
+
+	fmt.Println(len(notes))
+
+	fmt.Println()
+
+	for i := 0; i < len(notes); i++ {
+		fmt.Println(notes[i])
+	}
+
+	for _, note := range notes {
+		fmt.Println(note)
+	}
 }
