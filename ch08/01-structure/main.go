0278a752e24172bb0c241a13d8408ce69b83a08a
dev-t2 <oo_ri@naver.com>
2023-10-03 03:04:27 +0900

    Chapter 08 > 01. Structure Update

--- ch08/01-structure/main.go
+++ ch08/01-structure/main.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+type car struct {
+	name     string
+	topSpeed float64
+}
+
 func main() {
 	var myStruct struct {
 		num    float64
@@ -18,4 +23,14 @@ func main() {
 	myStruct.toggle = true
 
 	fmt.Println(myStruct.num, myStruct.word, myStruct.toggle)
+
+	fmt.Println()
+
+	var ev6 car
+
+	ev6.name = "KIA EV6 GT"
+	ev6.topSpeed = 260
+
+	fmt.Println("Name:", ev6.name)
+	fmt.Println("Top Speed:", ev6.topSpeed)
 }
