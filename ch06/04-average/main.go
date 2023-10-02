34180ff79e860b8902098f552e0a0e9f199a7b72
dev-t2 <oo_ri@naver.com>
2023-10-02 14:16:54 +0900

    Chapter 06 > 04. Average Update

--- ch06/04-average/main.go
+++ ch06/04-average/main.go
@@ -2,24 +2,26 @@ package main
 
 import (
 	"fmt"
-	"learn-go/packages/datafile"
 	"log"
+	"os"
+	"strconv"
 )
 
 func main() {
-	nums, err := datafile.GetFloats("ch06/04-average/data.txt")
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
+	arguments := os.Args[1:]
 	sum := 0.0
 
-	for _, num := range nums {
+	for _, argument := range arguments {
+		num, err := strconv.ParseFloat(argument, 64)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		
 		sum += num
 	}
 
-	length := float64(len(nums))
+	length := float64(len(arguments))
 
 	fmt.Printf("Average: %0.2f\n", sum / length)
 }
