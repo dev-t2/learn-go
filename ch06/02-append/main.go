394e9e0431a38517a03dcee87cb9314025cd78bd
dev-t2 <oo_ri@naver.com>
2023-10-02 13:08:28 +0900

    Chapter 06 > 02. Append Update

--- ch06/02-append/main.go
+++ ch06/02-append/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	slice := []string{"a", "b"}
+
+	fmt.Println(slice, len(slice))
+
+	slice = append(slice, "c")
+
+	fmt.Println(slice, len(slice))
+
+	slice = append(slice, "d", "e")
+
+	fmt.Println(slice, len(slice))
 }
