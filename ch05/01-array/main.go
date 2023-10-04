f02a96cd0a3dc9e32eae4d73d7aa299aae348dfe
dev-t2 <oo_ri@naver.com>
2023-10-05 00:19:32 +0900

    Chapter 05 > 01. Array Code Refactoring

--- ch05/01-array/main.go
+++ ch05/01-array/main.go
@@ -5,8 +5,8 @@ import "fmt"
 func main() {
 	var nums [5]int
 
-	nums[0] = 2
-	nums[1] = 3
+	nums[2] = 2
+	nums[4] = 4
 
 	fmt.Println(nums[0], nums[2], nums[4])
 
@@ -20,17 +20,7 @@ func main() {
 
 	fmt.Println()
 
-	// var notes [7]string = [7]string{"do", "re", "mi", "fa", "so", "la", "ti"}
-
-	notes := [7]string{
-		"do", 
-		"re", 
-		"mi", 
-		"fa", 
-		"so", 
-		"la", 
-		"ti",
-	}
+	notes := [7]string{"do", "re", "mi", "fa", "so", "la", "ti"}
 
 	fmt.Println(notes[0], notes[3], notes[6])
 
@@ -42,7 +32,7 @@ func main() {
 	fmt.Println()
 
 	for i := 0; i < len(notes); i++ {
-		fmt.Println(i, notes[i])
+		fmt.Println(notes[i])
 	}
 
 	fmt.Println()
