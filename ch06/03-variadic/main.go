479ef2d85961d64f2817115e3e85c4849b62cb88
dev-t2 <oo_ri@naver.com>
2023-10-11 00:01:10 +0900

    Chapter 06 > 03. Variadic Code Refactoring

--- ch06/03-variadic/main.go
+++ ch06/03-variadic/main.go
@@ -21,17 +21,7 @@ func maximum(nums ...float64) float64 {
 	return max
 }
 
-func isRange(min, max float64, nums ...float64) []float64 {
-	var results []float64
 
-	for _, num := range nums {
-		if num >= min && num <= max {
-			results = append(results, num)
-		}
-	}
-
-	return results
-}
 
 func main() {
 	severalInts(1, 2, 3)
@@ -44,9 +34,4 @@ func main() {
 
 	fmt.Println(maximum(71.8, 56.2, 89.5))
 	fmt.Println(maximum(90.7, 89.7, 98.5, 92.3))
-
-	fmt.Println()
-
-	fmt.Println(isRange(1, 100, -12.5, 3.2, 0, 50, 103.5))
-	fmt.Println(isRange(-10, 10, 4.1, 12, -12, -5.2))
 }
