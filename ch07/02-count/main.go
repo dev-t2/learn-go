4a431f9c29f9ce1ab5ef70534a26cb826ac56506
dev-t2 <oo_ri@naver.com>
2023-10-11 21:47:51 +0900

    Chapter 07 > 02. Count Code Refactoring

--- ch07/02-count/main.go
+++ ch07/02-count/main.go
@@ -1,13 +1,44 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"learn-go/ch07/02-count/datafile"
 	"log"
+	"os"
 )
 
+func getStrings(filename string) ([]string, error) {
+	file, err := os.Open(filename)
+
+	if err != nil {
+		return nil, err
+	}
+	
+	scanner := bufio.NewScanner(file)
+
+	var lines []string
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		lines = append(lines, line)
+	}
+
+	err = file.Close()
+
+	if err != nil {
+		return nil, err
+	}
+
+	if scanner.Err() != nil {
+		return nil, scanner.Err()
+	}
+
+	return lines, nil
+}
+
 func main() {
-	lines, err := datafile.GetStrings("ch07/02-count/votes.txt")
+	lines, err := getStrings("ch07/02-count/votes.txt")
 
 	if err != nil {
 		log.Fatal(err)
