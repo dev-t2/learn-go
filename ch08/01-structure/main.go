8311a38468280068e8ac7315b14b8b7517ec2af2
dev-t2 <oo_ri@naver.com>
2023-10-14 13:06:01 +0900

    Chapter 08 > 01. Structure Update

--- ch08/01-structure/main.go
+++ ch08/01-structure/main.go
@@ -10,4 +10,10 @@ func main() {
 	}
 
 	fmt.Printf("%#v\n", myStruct)
+
+	myStruct.number = 3.14
+	myStruct.word = "pie"
+	myStruct.toggle = true
+
+	fmt.Println(myStruct.number, myStruct.word, myStruct.toggle)
 }
