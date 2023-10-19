1555161092bdb9b7393d1a0249bf06a66a92908f
dev-t2 <oo_ri@naver.com>
2023-10-19 23:51:10 +0900

    Chapter 09 > 03. Method Update

--- ch09/03-method/main.go
+++ ch09/03-method/main.go
@@ -20,6 +20,20 @@ func (m MyType) ExportedMethod() {}
 
 func (m MyType) unexportedMethod() {}
 
+func (m MyType) method() {
+	fmt.Println("Method with value receiver")
+}
+
+func (m *MyType) pointerMethod() {
+	fmt.Println("Method with pointer receiver")
+}
+
+type Number int
+
+func (n *Number) double() {
+	*n *= 2
+}
+
 func main() {
 	value1 := MyType("Value1")
 
@@ -32,4 +46,30 @@ func main() {
 	value2.MethodWithParameters(4)
 
 	fmt.Println(value2.WithReturn())
+
+	fmt.Println()
+
+	value3 := MyType("Value 3")
+
+	value3.method()
+	(&value3).pointerMethod()
+	value3.pointerMethod()
+
+	fmt.Println()
+
+	pointer := &value3
+
+	(*pointer).method()
+	pointer.method()
+	pointer.pointerMethod()
+
+	fmt.Println()
+
+	num := Number(4)
+	
+	fmt.Println(num)
+
+	num.double()
+
+	fmt.Println(num)
 }
