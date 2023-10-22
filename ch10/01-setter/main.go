2eaf85631ac2a5d8c0dbd22c3215a67a6c95d971
dev-t2 <oo_ri@naver.com>
2023-10-22 20:29:57 +0900

    Chapter 10 > Packages Update

--- ch10/01-setter/main.go
+++ ch10/01-setter/main.go
@@ -1,59 +1,27 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"learn-go/ch10/packages/calendar"
 	"log"
 )
 
-type Date struct {
-	Year  int
-	Month int
-	Day   int
-}
 
-func (d *Date) SetYear(year int) error {
-	if year < 1989 || year > 2057 {
-		return errors.New("Invalid Year")
-	}
-
-	d.Year = year
-
-	return nil
-}
-
-func (d *Date) SetMonth(month int) error {
-	if month < 1 || month > 12 {
-		return errors.New("Invalid Month")
-	}
-
-	d.Month = month
-
-	return nil
-}
-
-func (d *Date) SetDay(day int) error {
-	if day < 1 || day > 31 {
-		return errors.New("Invalid Day")
-	}
-
-	d.Day = day
-
-	return nil
-}
 
 func main() {
-	date := Date{Year: 2023, Month: 10, Day: 21}
-
-	fmt.Println(date)
-
-	date = Date{Year: 0, Month: 0, Day: 0}
+	date := calendar.Date{Year: 2023, Month: 10, Day: 21}
 
 	fmt.Println(date)
 
 	fmt.Println()
 
-	date = Date{}
+	date = calendar.Date{Year: 0, Month: 0, Day: 0}
+
+	fmt.Println(date)
+
+	fmt.Println()
+
+	date = calendar.Date{}
 
 	err := date.SetYear(2023)
 
