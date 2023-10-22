482163dce92da6c9d49cd2e38bfc53fe047fda81
dev-t2 <oo_ri@naver.com>
2023-10-22 10:04:40 +0900

    Chapter 10. > 01. Setter Update

--- ch10/01-setter/main.go
+++ ch10/01-setter/main.go
@@ -8,12 +8,34 @@ type Date struct {
 	Day   int
 }
 
+func (d *Date) SetYear(year int) {
+	d.Year = year
+}
+
+func (d *Date) SetMonth(month int) {
+	d.Month = month
+}
+
+func (d *Date) SetDay(day int) {
+	d.Day = day
+}
+
 func main() {
 	date := Date{Year: 2023, Month: 10, Day: 21}
 
 	fmt.Println(date)
 
 	date = Date{Year: 0, Month: 0, Day: 0}
+
+	fmt.Println(date)
+
+	fmt.Println()
+
+	date = Date{}
+
+	date.SetYear(0)
+	date.SetMonth(0)
+	date.SetDay(0)
 
 	fmt.Println(date)
 }
