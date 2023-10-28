caf5d2c5a9a190a41b6aa2a2302fd07fd12515f7
dev-t2 <oo_ri@naver.com>
2023-10-28 14:47:16 +0900

    12. Interfaces > 1. Interface Update

--- 12-interfaces/1-interface/main.go
+++ 12-interfaces/1-interface/main.go
@@ -5,6 +5,22 @@ import (
 	"learn-go/12-interfaces/mypackage"
 )
 
+type NoiseMaker interface {
+	MakeSound()
+}
+
+type Whistle string
+
+func (w Whistle) MakeSound() {
+	fmt.Println("Whistle")
+}
+
+type Horn string
+
+func (h Horn) MakeSound() {
+	fmt.Println("Horn")
+}
+
 func main() {
 	var value mypackage.MyInterface
 
@@ -13,4 +29,16 @@ func main() {
 	value.MethodWithoutParameter()
 	value.MethodWithParameter(123.4)
 	fmt.Println(value.MethodWithReturn())
+
+	fmt.Println()
+
+	var toy NoiseMaker
+
+	toy = Whistle("Whistle")
+
+	toy.MakeSound()
+
+	toy = Horn("Horn")
+
+	toy.MakeSound()
 }
