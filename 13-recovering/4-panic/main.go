68f5695be43caa3f8357e0a13d3c63a13cfc8cd0
dev-t2 <oo_ri@naver.com>
2023-10-29 14:51:51 +0900

    13. Recovering > 4. Panic Update

--- 13-recovering/4-panic/main.go
+++ 13-recovering/4-panic/main.go
@@ -1,14 +1,22 @@
 package main
 
+import "fmt"
+
 func three() {
+	defer fmt.Println("Deferred in three()")
+
 	panic("This call stack's too deep for me")
 }
 
 func two() {
+	defer fmt.Println("Deferred in two()")
+
 	three()
 }
 
 func one() {
+	defer fmt.Println("Deferred in one()")
+
 	two()
 }
 
