45f577af61fb91f53265c1a82ea789f78fdb5e4d
dev-t2 <oo_ri@naver.com>
2023-10-29 14:54:42 +0900

    13. Recovering > 5. Files Update

--- 13-recovering/5-files/main.go
+++ 13-recovering/5-files/main.go
@@ -2,41 +2,30 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"path/filepath"
 )
 
-func scanDirectory(path string) error {
+func scanDirectory(path string) {
 	fmt.Println(path)
 
 	files, err := os.ReadDir(path)
 
 	if err != nil {
-		return err
+		panic(err)
 	}
 
 	for _, file := range files {
 		filePath := filepath.Join(path, file.Name())
 
 		if file.IsDir() {
-			err := scanDirectory(filePath)
-
-			if err != nil {
-				return err
-			}
+			scanDirectory(filePath)
 		} else {
 			fmt.Println(filePath)
 		}
 	}
-
-	return nil
 }
 
 func main() {
-	err := scanDirectory("13-recovering\\5-files\\packages")
-
-	if err != nil {
-		log.Fatal(err)
-	}
+	scanDirectory("13-recovering\\5-files\\packages")
 }
