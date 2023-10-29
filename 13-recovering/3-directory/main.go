85bbf0b557d33ef51e4801311355656030c7c424
dev-t2 <oo_ri@naver.com>
2023-10-29 09:58:29 +0900

    13. Recovering > 3. Directory Update

---
+++ 13-recovering/3-directory/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	files, err := os.ReadDir("13-recovering/3-directory/directories")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		if file.IsDir() {
+			fmt.Println("Directory:", file.Name())
+		} else {
+			fmt.Println("File:", file.Name())
+		}
+	}
+}
