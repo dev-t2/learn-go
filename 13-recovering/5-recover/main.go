2905bf8696a4154fe6138981d991df04b8efa87e
dev-t2 <oo_ri@naver.com>
2023-10-29 15:23:40 +0900

    13. Recovering > 5. Recover Update

--- 13-recovering/5-recover/main.go
+++ 13-recovering/5-recover/main.go
@@ -3,13 +3,21 @@ package main
 import "fmt"
 
 func calmDown() {
-	fmt.Println("Recover:", recover())
+	r := recover()
+
+	err, ok := r.(error)
+
+	if ok {
+		fmt.Println(err.Error())
+	}
 }
 
 func breakOut() {
 	defer calmDown()
 
-	panic("Break Out")
+	err := fmt.Errorf("Break Out")
+
+	panic(err)
 }
 
 func main() {
