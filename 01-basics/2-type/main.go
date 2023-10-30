01ec0a695dd8af120036a4bfa7a3ccb647a65143
dev-t2 <oo_ri@naver.com>
2023-10-30 23:10:02 +0900

    01. Basics > 2. Type Update

--- 01-basics/2-type/main.go
+++ 01-basics/2-type/main.go
@@ -6,12 +6,21 @@ import (
 )
 
 func main() {
+	fmt.Println('1')
 	fmt.Println('A')
 	fmt.Println('b')
 	fmt.Println('가')
-	fmt.Println('\t')
+	fmt.Println('"')
 	fmt.Println('\n')
-	fmt.Println('\\')
+
+	fmt.Println()
+
+	fmt.Println("1")
+	fmt.Println("A")
+	fmt.Println("b")
+	fmt.Println("가")
+	fmt.Println("\"")
+	// fmt.Println("\n")
 
 	fmt.Println()
 
@@ -20,29 +29,30 @@ func main() {
 
 	fmt.Println()
 
-	fmt.Println(42)
+	fmt.Println(15)
 	fmt.Println(3.141592)
 
 	fmt.Println()
 
 	fmt.Println(1 + 2)
-	fmt.Println(5.4 - 2.2)
+	fmt.Println(3.4 - 1.2)
 	fmt.Println(3 * 4)
 	fmt.Println(7.5 / 5)
 
 	fmt.Println()
 
-	fmt.Println(4 < 6)
-	fmt.Println(4 > 6)
-	fmt.Println(2 + 2 == 5)
-	fmt.Println(2 + 2 != 5)
-	fmt.Println(4 <= 6)
-	fmt.Println(4 >= 4)
+	fmt.Println(1 < 5)
+	fmt.Println(1 > 5)
+	fmt.Println(1 <= 5)
+	fmt.Println(5 >= 5)
+	fmt.Println(1 + 2 == 5)
+	fmt.Println(1 + 2 != 5)
 
 	fmt.Println()
 
+	fmt.Println(reflect.TypeOf('A'))
 	fmt.Println(reflect.TypeOf("Hello, Go!"))
 	fmt.Println(reflect.TypeOf(true))
-	fmt.Println(reflect.TypeOf(42))
+	fmt.Println(reflect.TypeOf(15))
 	fmt.Println(reflect.TypeOf(3.141592))
 }
