bf41fa81c9e3c5f5c3430b20a42442db5e241cd8
dev-t2 <oo_ri@naver.com>
2023-10-31 21:34:37 +0900

    03. Loops > 2. Guess Update

--- 03-loops/2-guess/main.go
+++ 03-loops/2-guess/main.go
@@ -12,33 +12,35 @@ import (
 
 func main() {
 	target := rand.Intn(100) + 1
-	isSuccess := false
-
-	fmt.Println("\nI've chosen a random number between 1 and 100")
+	
+	fmt.Println("I've chosen a random number between 1 and 100")
 	fmt.Println("Can you guess it?")
 
+	fmt.Println()
+	
 	reader := bufio.NewReader(os.Stdin)
+	isSuccess := false
 
 	for guesses := 0; guesses < 10; guesses++ {
-		fmt.Println("\nYou have", 10 - guesses, "guesses left")
+		fmt.Println("You have", 10 - guesses, "guesses left")
 		fmt.Print("Make a guess: ")
 
-		input, err := reader.ReadString('\n')
+		str, err := reader.ReadString('\n')
 	
 		if err != nil {
 			log.Fatal(err)
 		}
 	
-		input = strings.TrimSpace(input)
-		guess, err := strconv.Atoi(input)
+		str = strings.TrimSpace(str)
+		num, err := strconv.Atoi(str)
 	
 		if err != nil {
 			log.Fatal(err)
 		}
 	
-		if guess < target {
+		if num < target {
 			fmt.Println("Your guess was LOW")
-		} else if guess > target {
+		} else if num > target {
 			fmt.Println("Your guess was HIGH")
 		} else {
 			isSuccess = true
