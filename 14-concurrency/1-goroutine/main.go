bac6ed214798b02f126beef1960a03acd253735b
dev-t2 <oo_ri@naver.com>
2023-11-02 23:39:27 +0900

    14. Concurrency > 1. Goroutine Update

---
+++ 14-concurrency/1-goroutine/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func a() {
+	for i := 0; i < 50; i++ {
+		fmt.Print("a")
+	}
+}
+
+func b() {
+	for i := 0; i < 50; i++ {
+		fmt.Print("b")
+	}
+}
+
+func main() {
+	fmt.Print("start ")
+
+	go a()
+	go b()
+
+	time.Sleep(time.Second)
+
+	fmt.Println(" end")
+}
