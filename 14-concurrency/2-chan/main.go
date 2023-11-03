173613a9ee4ed4d981e3bd02aef4c42356c7047d
dev-t2 <oo_ri@naver.com>
2023-11-04 00:14:23 +0900

    14. Concurrency > 2. Chan Update

--- 14-concurrency/2-chan/main.go
+++ 14-concurrency/2-chan/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func abc(channel chan string) {
 	channel <- "a"
@@ -12,6 +15,28 @@ func def(channel chan string) {
 	channel <- "d"
 	channel <- "e"
 	channel <- "f"
+}
+
+func report(name string, delay int) {
+	for i := 0; i < delay; i++ {
+		fmt.Println(name, "stop")
+
+		time.Sleep(time.Second)
+	}
+
+	fmt.Println(name, "start")
+}
+
+func send(channel chan string) {
+	report("send", 2)
+
+	fmt.Println("*** value ***")
+
+	channel <- "a"
+
+	fmt.Println("*** value ***")
+
+	channel <- "b"
 }
 
 func main() {
@@ -27,4 +52,15 @@ func main() {
 	fmt.Println(<-channel2)
 	fmt.Println(<-channel1)
 	fmt.Println(<-channel2)
+
+	fmt.Println()
+
+	channel3 := make(chan string)
+
+	go send(channel3)
+
+	report("receive", 5)
+
+	fmt.Println(<-channel3)
+	fmt.Println(<-channel3)
 }
