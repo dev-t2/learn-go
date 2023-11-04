fb6eba9cb5c2d94f2b7cfd50ae201d6b3d21bfed
dev-t2 <oo_ri@naver.com>
2023-11-04 13:48:04 +0900

    16. Web > 1. App Update

---
+++ 16-web/1-app/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func viewHandler(writer http.ResponseWriter, request *http.Request) {
+	message := []byte("Hello, Web!")
+
+	_, err := writer.Write(message)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	http.HandleFunc("/hello", viewHandler)
+
+	err := http.ListenAndServe("localhost:8080", nil)
+
+	log.Fatal(err)
+}
