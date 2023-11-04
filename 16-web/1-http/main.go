777f2fc1e626c671273e9d33e65baa059841ca22
dev-t2 <oo_ri@naver.com>
2023-11-04 15:13:23 +0900

    16. Web > 1. HTTP Update

--- 16-web/1-http/main.go
+++ 16-web/1-http/main.go
@@ -5,18 +5,32 @@ import (
 	"net/http"
 )
 
-func viewHandler(writer http.ResponseWriter, request *http.Request) {
-	message := []byte("Hello, Web!")
-
-	_, err := writer.Write(message)
+func write(res http.ResponseWriter, message string) {
+	_, err := res.Write([]byte(message))
 
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
+func englishHandler(res http.ResponseWriter, req *http.Request) {
+	write(res, "Hello, Web!")
+}
+
+func frenchHandler(res http.ResponseWriter, req *http.Request) {
+	write(res, "Salut, Web!")
+}
+
+func hindiHandler(res http.ResponseWriter, req *http.Request) {
+	write(res, "Namaste, Web!")
+}
+
 func main() {
-	http.HandleFunc("/hello", viewHandler)
+	http.HandleFunc("/hello", englishHandler)
+
+	http.HandleFunc("/salut", frenchHandler)
+
+	http.HandleFunc("/namaste", hindiHandler)
 
 	err := http.ListenAndServe("localhost:8080", nil)
 
