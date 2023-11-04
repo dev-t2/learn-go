57574e87521c7fef35c8695a678c31d7f395b7c1
dev-t2 <oo_ri@naver.com>
2023-11-04 12:13:50 +0900

    15. Testing > 1. Join Update

--- 15-testing/1-join/main_test.go
+++ 15-testing/1-join/main_test.go
@@ -5,6 +5,11 @@ import (
 	"testing"
 )
 
+type TestData struct {
+	list 				[]string
+	resultValue string
+}
+
 func errorString(list []string, returnValue, resultValue string) string {
 	return fmt.Sprintf("JoinWithCommas(%#v) => \"%s\" != \"%s\"", list, returnValue, resultValue) 
 }
@@ -36,5 +41,21 @@ func TestThreeElements(t *testing.T) {
 
 	if returnValue != resultValue {
 		t.Errorf(errorString(list, returnValue, resultValue))
+	}
+}
+
+func TestJoinWithCommas(t * testing.T) {
+	tests := []TestData{
+		{list: []string{"Austin"}, resultValue: "Austin"},
+		{list: []string{"Austin", "Chloe"}, resultValue: "Austin and Chloe"},
+		{list: []string{"Austin", "Chloe", "Sally"}, resultValue: "Austin, Chloe, and Sally"},
+	}
+
+	for _, test := range tests {
+		returnValue := JoinWithCommas(test.list)
+
+		if returnValue != test.resultValue {
+			t.Errorf(errorString(test.list, returnValue, test.resultValue))
+		}
 	}
 }
