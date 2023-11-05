83bef4e8fdca190bd06b68c3aecab13cb4bb9fda
dev-t2 <oo_ri@naver.com>
2023-11-05 11:40:21 +0900

    15. Web > 3. Text Update

--- 15-web/3-text/main.go
+++ 15-web/3-text/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"text/template"
@@ -19,6 +20,30 @@ func main() {
 	check(err)
 
 	err = tmpl.Execute(os.Stdout, nil)
+
+	check(err)
+
+	fmt.Println()
+
+	text = "Template Start\nAction: {{.}}\nTemplate End\n"
+
+	tmpl, err = template.New("test").Parse(text)
+
+	check(err)
+
+	err = tmpl.Execute(os.Stdout, "A")
+
+	check(err)
+
+	fmt.Println()
+
+	err = tmpl.Execute(os.Stdout, 15)
+
+	check(err)
+
+	fmt.Println()
+
+	err = tmpl.Execute(os.Stdout, true)
 
 	check(err)
 }
