1082794db7131cf4a4ccca6a70d2dccb2a0677b2
dev-t2 <oo_ri@naver.com>
2023-11-05 10:34:28 +0900

    15. Web > 3. Text Update

--- 15-web/3-text/main.go
+++ 15-web/3-text/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
+	"text/template"
+)
 
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	text := "Text Template\n"
+	tmpl, err := template.New("test").Parse(text)
+
+	check(err)
+
+	err = tmpl.Execute(os.Stdout, nil)
+
+	check(err)
 }
