f7f881b5a9e55c789422699eae6abcb132d1bb64
dev-t2 <oo_ri@naver.com>
2023-11-05 23:40:25 +0900

    15. Web > 5. File Update

---
+++ 15-web/5-file/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+	"os"
+)
+
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	file, err := os.OpenFile("15-web/5-file/data.txt", os.O_WRONLY, os.FileMode(0600))
+
+	check(err)
+
+	_, err = file.Write([]byte("Chloe\n"))
+
+	check(err)
+
+	err = file.Close()
+
+	check(err)
+}
