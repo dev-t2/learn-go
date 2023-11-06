dfd5966572137ae2036c492f6260cebbc2c478b1
dev-t2 <oo_ri@naver.com>
2023-11-06 23:57:17 +0900

    15. Web > 5. Operator Update

--- 15-web/5-operator/main.go
+++ 15-web/5-operator/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Printf("0 & 0 == %b\n", 0 & 0)
+	fmt.Printf("1 & 0 == %b\n", 1 & 0)
+	fmt.Printf("1 & 1 == %b\n", 1 & 1)
+
+	fmt.Println()
+
+	fmt.Printf("%02b\n", 1)
+	fmt.Printf("%02b\n", 3)
+	fmt.Printf("%02b\n", 1 & 3)
+
+	fmt.Println()
+
+	fmt.Printf("%02b\n", 2)
+	fmt.Printf("%02b\n", 3)
+	fmt.Printf("%02b\n", 2 & 3)
 }
