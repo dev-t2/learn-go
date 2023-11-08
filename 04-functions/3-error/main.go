7dd528ac6e5ecb7cad0d97e656ad92bbd85dd2e9
dev-t2 <oo_ri@naver.com>
2023-11-08 21:15:17 +0900

    04. Functions > 3. Error Update

--- 04-functions/3-error/main.go
+++ 04-functions/3-error/main.go
@@ -3,23 +3,14 @@ package main
 import (
 	"errors"
 	"fmt"
-	"math"
 )
 
-func divide(dividend, divisor float64) (float64, error) {
-	if divisor == 0 {
+func divide(a, b float64) (float64, error) {
+	if b == 0 {
 		return 0, errors.New("Can't divide by 0")
 	}
 
-	return dividend / divisor, nil
-}
-
-func getSquareRoot(num float64) (float64, error) {
-	if num < 0 {
-		return 0, fmt.Errorf("Can't square root of negative number")
-	}
-
-	return math.Sqrt(num), nil
+	return a / b, nil
 }
 
 func main() {
@@ -30,22 +21,14 @@ func main() {
 
 	fmt.Println()
 
-	err = fmt.Errorf("Height of %.2f is invalid", -1.23456)
+	err = fmt.Errorf("Height of %.2f is invalid", -1.23456789)
 
 	fmt.Println(err.Error())
 	fmt.Println(err)
 
 	fmt.Println()
 
-	result, err := divide(1.23456, 0)
-
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Printf("%.2f\n", result)
-	}
-
-	result, err = getSquareRoot(-1.23456)
+	result, err := divide(15, 0)
 
 	if err != nil {
 		fmt.Println(err)
