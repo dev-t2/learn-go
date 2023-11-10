b51f50b76b99caa527defe3c05b8ede693717f0e
dev-t2 <oo_ri@naver.com>
2023-11-10 23:14:57 +0900

    07. Slices > 5. Rune Update

---
+++ 07-slices/5-rune/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
+func main() {
+	asciiString := "ABCDE"
+	utf8String := "가나다라마"
+
+	fmt.Println(len(asciiString))
+	fmt.Println(len(utf8String))
+
+	fmt.Println()
+
+	fmt.Println(utf8.RuneCountInString(asciiString))
+	fmt.Println(utf8.RuneCountInString(utf8String))
+
+	fmt.Println()
+
+	asciiBytes := []byte(asciiString)
+	utf8Bytes := []byte(utf8String)
+
+	asciiBytesPartial := asciiBytes[3:]
+	utf8BytesPartial := utf8Bytes[3:]
+
+	fmt.Println(string(asciiBytesPartial))
+	fmt.Println(string(utf8BytesPartial))
+
+	fmt.Println()
+
+	asciiRunes := []rune(asciiString)
+	utf8Runes := []rune(utf8String)
+
+	asciiRunesPartial := asciiRunes[3:]
+	utf8RunesPartial := utf8Runes[3:]
+
+	fmt.Println(string(asciiRunesPartial))
+	fmt.Println(string(utf8RunesPartial))
+}
