185820810693e841f7501362b825fb9160cfbdbf
dev-t2 <oo_ri@naver.com>
2023-11-10 22:56:18 +0900

    02. Conditionals > 4. Switch Update

---
+++ 02-conditionals/4-switch/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	switch rand.Intn(3) + 1 {
+		case 1:
+			fmt.Println("Number 1")
+		case 2:
+			fmt.Println("Number 2")
+		case 3:
+			fmt.Println("Number 3")
+		default:
+			panic("Invalid Number")		
+	} 
+
+}
