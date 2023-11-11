acf2d0cb46177506d4b67ef45a9ad8b190568b73
dev-t2 <oo_ri@naver.com>
2023-11-11 15:43:29 +0900

    14. Concurrency > 4. Checker Update

--- 14-concurrency/4-checker/main.go
+++ 14-concurrency/4-checker/main.go
@@ -7,11 +7,13 @@ import (
 )
 
 func hitUrl(url string) error {
-	fmt.Println(url)
+	fmt.Println("Checking:", url)
 
 	res, err := http.Get(url)
 
 	if err != nil || res.StatusCode >= 400{
+		fmt.Println(err, res.StatusCode)
+
 		return errors.New("Request Failed")
 	}
 
@@ -25,10 +27,27 @@ func main() {
 		"https://www.youtube.com",
 		"https://www.wavve.com",
 		"https://github.com",
+		"https://www.inflearn.com",
+		"https://nomadcoders.co",
+		"https://fastcampus.co.kr",
 		"https://go.dev",
 	}
 
+	results := make(map[string]string)
+
 	for _, url := range urls {
-		hitUrl(url)
+		err := hitUrl(url)
+
+		if err != nil {
+			results[url] = "Failure"
+		} else {
+			results[url] = "Success"
+		}
+	}
+
+	fmt.Println()
+
+	for url, result := range results {
+		fmt.Println(url, result)
 	}
 }
