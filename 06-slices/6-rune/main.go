8b48ead1e5f0e180919ce1ef5857fc57612405ee
dev-t2 <oo_ri@naver.com>
2023-11-22 21:22:27 +0900

    06. Slices > 6. Rune Update

--- 06-slices/6-rune/main.go
+++ 06-slices/6-rune/main.go
@@ -9,13 +9,13 @@ func main() {
 	asciiString := "ABCDE"
 	utf8String := "가나다라마"
 
-	fmt.Println(len(asciiString))
-	fmt.Println(len(utf8String))
+	fmt.Println(len(asciiString)) // 5
+	fmt.Println(len(utf8String)) // 15
 
 	fmt.Println()
 
-	fmt.Println(utf8.RuneCountInString(asciiString))
-	fmt.Println(utf8.RuneCountInString(utf8String))
+	fmt.Println(utf8.RuneCountInString(asciiString)) // 5
+	fmt.Println(utf8.RuneCountInString(utf8String)) // 5
 
 	fmt.Println()
 
@@ -25,8 +25,8 @@ func main() {
 	asciiBytesPartial := asciiBytes[3:]
 	utf8BytesPartial := utf8Bytes[3:]
 
-	fmt.Println(string(asciiBytesPartial))
-	fmt.Println(string(utf8BytesPartial))
+	fmt.Println(string(asciiBytesPartial)) // DE
+	fmt.Println(string(utf8BytesPartial)) // 나다라마
 
 	fmt.Println()
 
@@ -36,30 +36,30 @@ func main() {
 	asciiRunesPartial := asciiRunes[3:]
 	utf8RunesPartial := utf8Runes[3:]
 
-	fmt.Println(string(asciiRunesPartial))
-	fmt.Println(string(utf8RunesPartial))
+	fmt.Println(string(asciiRunesPartial)) // DE
+	fmt.Println(string(utf8RunesPartial)) // 라마
 
 	fmt.Println()
 
 	for index, currentByte := range asciiBytes {
-		fmt.Printf("%d: %s\n", index, string(currentByte))
+		fmt.Printf("%d: %s\n", index, string(currentByte)) // 0: A, 1: B, 2: C, 3: D, 4: E
 	}
 
 	fmt.Println()
 
 	for index, currentByte := range utf8Bytes {
-		fmt.Printf("%d: %s\n", index, string(currentByte))
+		fmt.Printf("%d: %s\n", index, string(currentByte)) // Length: 15
 	}
 
 	fmt.Println()
 
 	for index, currentRune := range asciiRunes {
-		fmt.Printf("%d: %s\n", index, string(currentRune))
+		fmt.Printf("%d: %s\n", index, string(currentRune)) // 0: A, 1: B, 2: C, 3: D, 4: E
 	}
 
 	fmt.Println()
 
 	for index, currentRune := range utf8Runes {
-		fmt.Printf("%d: %s\n", index, string(currentRune))
+		fmt.Printf("%d: %s\n", index, string(currentRune)) // 0: 가, 1: 나, 2: 다, 3: 라, 4: 마
 	}
 }
