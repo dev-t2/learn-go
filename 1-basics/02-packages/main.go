8a5e8183aa225f9440202b70f71dff20c414c3b1
dev-t2 <oo_ri@naver.com>
2023-12-04 23:41:38 +0900

    1. Basics Folders Refactoring

--- 1-basics/02-packages/main.go
+++ 1-basics/02-packages/main.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
 )
 
 func main() {
 	fmt.Println("My favorite number is", rand.Intn(10))
+
+	fmt.Println()
+
+	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
+
+	fmt.Println()
+
+	fmt.Println(math.Pi)
 }
