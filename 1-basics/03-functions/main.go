0d851866578d8957e81811b654f4d34c55c52a84
dev-t2 <oo_ri@naver.com>
2023-12-04 23:45:11 +0900

    1. Basics Folders Refactoring

--- 1-basics/03-functions/main.go
+++ 1-basics/03-functions/main.go
@@ -10,6 +10,27 @@ func add(x, y int) int {
 	return x + y
 }
 
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+
+	return
+}
+
 func main() {
 	fmt.Println(add(42, 13))
+
+	fmt.Println()
+
+	a, b := swap("hello", "world")
+
+	fmt.Println(a, b)
+
+	fmt.Println()
+
+	fmt.Println(split(17))
 }
