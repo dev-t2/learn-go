7b0de989724aa27878751e175e6acf5e7bbaa3a9
dev-t2 <oo_ri@naver.com>
2023-12-04 23:52:25 +0900

    1. Basics Folders Refactoring

--- 1-basics/04-variables/main.go
+++ 1-basics/04-variables/main.go
@@ -2,10 +2,27 @@ package main
 
 import "fmt"
 
-var c, python, java bool
+var bool1, bool2 bool
+
+var num1, num2 int = 1, 2
 
 func main() {
-	var i int
+	var num3 int
 	
-	fmt.Println(i, c, python, java)
+	fmt.Println(num3, bool1, bool2)
+
+	fmt.Println()
+
+	var bool3, str1 = true, "string"
+
+	fmt.Println(num1, num2, bool3, str1)
+
+	fmt.Println()
+
+	var num4, num5 int = 1, 2
+	
+	num6 := 3
+	bool4, str2 := true, "string"
+
+	fmt.Println(num4, num5, num6, bool4, str2)
 }
